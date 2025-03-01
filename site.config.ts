418625dfc25ddaa21091436c4442cda6212dfc73
shiqi <you@example.com>
2025-03-02 00:55:27 +0800

    revise

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,24 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'Wu-Shiqi-1a973b5da35c80b2931ee9ae1dd3248a?pvs=4',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'WuShiqi Personal Website',
+  domain: 'nhttps://bevel-pocket-985.notion.site/Wu-Shiqi-1a973b5da35c80b2931ee9ae1dd3248a?pvs=4',
+  author: 'Wu Shiqi',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: '',
+  github: 'Shiqi-Wu',
+  linkedin: 'https://www.linkedin.com/in/shiqi-wu-0aa667234/',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
